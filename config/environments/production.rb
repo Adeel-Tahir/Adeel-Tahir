3da5e653920fc1b06dfc562d8562191e517c5ccb
Dev <muhammad.adeel@devsinc.com>
2021-10-27 18:16:58 +0500

    *change applications.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,7 +107,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { host: Rails.application.credentials.host.fetch(:production_host) }
+  # config.action_mailer.default_url_options = { host: Rails.application.credentials.host.fetch(:production_host) }
 
   Cloudinary.config do |config|
     config.cloud_name = Rails.application.credentials.cloudinary.fetch(:cloud_name)
